1d3edac3c5dd8959cd1f186a0ca4c8f897008f11
snezamha <snezamha@gmail.com>
2025-05-05 19:19:36 +0200

    Add shadcn with all components

---
+++ src/components/providers/direction-provider.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import { DirectionProvider as RadixDirProvider } from '@radix-ui/react-direction';
+
+const DirectionProvider = ({
+  direction,
+  children
+}: {
+  direction: 'ltr' | 'rtl';
+  children: React.ReactNode;
+}) => {
+  return <RadixDirProvider dir={direction}>{children}</RadixDirProvider>;
+};
+
+export default DirectionProvider;
